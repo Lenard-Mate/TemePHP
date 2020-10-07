89392689056688f6d7ef01d77889c5471b10e360
Leni <lenard.mate.98@gmail.com>
2020-10-07 20:40:26 +0300

    newcomit5

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?>
+<form action="https://shielded-fjord-73648.herokuapp.com/texttest.php" enctype="multipart/form-data" method="POST" > 
+    <input type="submit" name="join_queue" value="Join Queue">
+</form>
 
 </body>
 </html>
