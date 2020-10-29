ba34d73ae2f414d31b67b3e651fa1875b5345094
Leni <lenard.mate.98@gmail.com>
2020-10-29 02:16:53 +0200

    terst

---
+++ project2/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="https://shielded-fjord-73648.herokuapp.com/testtext.php" enctype="multipart/form-data" method="POST" > 
+    <input type="submit" name="join_queue" value="Join Queue">
+</form>
+
+</body>
+</html>
